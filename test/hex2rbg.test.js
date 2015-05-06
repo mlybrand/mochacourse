4c9614bebd3199b98d4be58caa5415a70f062125
unknown <lybm235@LNIDPC155254D.wads.res>
2015-05-06 11:41:55 -0700

    working on some sinon tests

--- test/hex2rbg.test.js
+++ test/hex2rbg.test.js
@@ -1,22 +1,45 @@
 var hex2rgb = require("../lib/hex2rgb");
 var expect = require("chai").expect;
+var sinon = require("sinon");
 
-describe("hex2rgb", function() {
+describe.only("hex2rgb", function() {
 
-	it("should return an error if the value is not a hex code", function(done) {
-		hex2rgb("blue", function(error, result) {
-			expect(error).to.exist;
-			done();
+	describe("convert method", function() {
+
+		it("should call parse once", function(done) {
+			sinon.spy(hex2rgb, "parse");
+			hex2rgb.convert("#ffffff", function(err, result) {
+				expect(hex2rgb.parse.calledOnce).to.be.true;
+				expect(hex2rgb.parse.args[0][0]).to.have.length(6);
+
+				hex2rgb.parse.restore();
+				done();
+			});
+		});
+
+		it("should always return the result of parse", function(done) {
+			sinon.stub(hex2rgb, "parse").returns([0,0,200]);
+			hex2rgb.convert("#abc", function(err, result) {
+				expect(result).to.deep.equal([0,0,200]);
+
+				hex2rgb.parse.restore();
+				done();
+			});
+		});
+
+		it("should return an error if the value is not a hex code", function(done) {
+			hex2rgb.convert("blue", function(error, result) {
+				expect(error).to.exist;
+				done();
+			});
+		});
+
+		it("should return a correctly converted rgb value", function(done) {
+			var rgb = hex2rgb.convert("#fff", function(error, result) {
+				expect(error).to.not.exist;
+				expect(result).to.deep.equal([255, 255, 255]);
+				done();
+			});
 		});
 	});
-
-	it("should return a correctly converted rgb value", function(done) {
-		var rgb = hex2rgb("#fff", function(error, result) {
-			expect(error).to.not.exist;
-			expect(result).to.deep.equal([255, 255, 255]);
-			done();
-		});
-	});
-
-	it("should return rgb if passed an rgb value");
 });
